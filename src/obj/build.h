b7852d4cf69b3d8de716f79e397b96e55e5b63ca
element94project <element94@gmail.com>
2014-04-29 07:13:47 -0500

    d

--- src/obj/build.h
+++
@@ -1,2 +0,0 @@
-// No build information available
-#define BUILD_DATE "2014-04-29 06:19:27 -0500"
--- src/obj/version.d
+++
@@ -1,6 +0,0 @@
-obj/version.o: version.cpp \
-  /Users/footballstar6226/Desktop/platinumbar-master/src/version.h \
-  /Users/footballstar6226/Desktop/platinumbar-master/src/obj/build.h
-version.cpp :
-  /Users/footballstar6226/Desktop/platinumbar-master/src/version.h :
-  /Users/footballstar6226/Desktop/platinumbar-master/src/obj/build.h :
